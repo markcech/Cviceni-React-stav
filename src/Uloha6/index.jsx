1f7a9a41c093992d82c0e16989c1ac95e7180aa3
Filip Chalupa <chalupa.filip@gmail.com>
2022-05-10 00:52:29 +0200

    Uloha6: přidán bonus s key

--- src/Uloha6/index.jsx
+++ src/Uloha6/index.jsx
@@ -5,6 +5,8 @@ import './ukol.css'
 // Zadání 2: V komponentě Ukol si nachystej proměnnou s hodnotou `false`. Klikem na tlačítko ji změň na `true` pro splněný úkol.
 // Zadání 3: Pokud je úkol splněný, nerenderuj tlačítko a `li` přidej navíc třídu `ukol--splnen`.
 
+// Bonus: Nastav jednotlivým položkám ve výpisu `key`, aby vývojářské nástroje v prohlížeči nezobrazovaly varování. Nápověda: Key musí být v seznamu unikátní. Použij název úkolu.
+
 const Ukol = ({ nazev }) => {
 	return (
 		<li className="ukol">
