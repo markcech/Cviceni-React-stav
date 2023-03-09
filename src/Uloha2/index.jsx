19193accaf02be9ae20d1b02caebeaded6b72464
Marketa Cechova <123273503+markcech@users.noreply.github.com>
2023-03-09 13:23:36 +0100

    Update index.jsx

--- src/Uloha2/index.jsx
+++ src/Uloha2/index.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 // Zadání 1: Převeď `cislo` na stavovou proměnnou.
 // Zadání 2: Nastav, aby jednotlivá tlačítka měnila stav. +1 přičítalo jedničku, +5 přičítalo pětku, vynulovat nastavovalo na nulu apod.
 
 const Pocitadlo = () => {
-	const cislo = 0
+
+		const [cislo, setCislo] = useState (0);
 
 	return (
 		<>
 			<h3>Počítadlo: {cislo}</h3>
-			<button>-5</button>
-			<button>-1</button>
-			<button>vynulovat</button>
-			<button>+1</button>
-			<button>+5</button>
+			<button onClick ={() => { setCislo(cislo-5) }}>-5</button>
+			<button onClick ={() => { setCislo(cislo-1) }}>-1</button>
+			<button onClick ={() => { setCislo(0) }}>vynulovat</button>
+			<button onClick ={() => { setCislo(cislo+1) }}>+1</button>
+			<button onClick ={() => { setCislo(cislo+5) }}>+5</button>
 		</>
 	)
 }
@@ -21,7 +22,6 @@ const Pocitadlo = () => {
 export const Uloha2 = () => {
 	return (
 		<>
-			<Pocitadlo />
 			<Pocitadlo />
 		</>
 	)
