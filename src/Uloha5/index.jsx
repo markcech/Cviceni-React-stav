e2540382167e8ffdb8dfc5c4a2765f15ccb727a8
Marketa Cechova <123273503+markcech@users.noreply.github.com>
2023-03-09 13:21:52 +0100

    Update index.jsx

--- src/Uloha5/index.jsx
+++ src/Uloha5/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './hamburger.css'
+import React, { useState } from "react";
+import "./hamburger.css";
 
 // Zadání 1: Přichystej si stavovou proměnnou s výchozí hodnotou `false`, ve které bude uloženo, jestli je menu otevřené nebo nikoliv.
 // Zadání 2: Po kliknutí na button změň stav z `true` na `false` nebo obráceně.
@@ -7,27 +7,38 @@ import './hamburger.css'
 // Zadání 4: Pokud je menu zavřené nech mu jen třídu `hamburger`. Pro otevřené přidej navíc `hamburger--otevrene`.
 
 export const Uloha5 = () => {
-	return (
-		<>
-			<button className="hamburger" aria-label="menu">
-				<span></span>
-				<span></span>
-				<span></span>
-			</button>
-			<ul>
-				<li>
-					<a href="#o-nas">O nás</a>
-				</li>
-				<li>
-					<a href="#co-delame">Co děláme</a>
-				</li>
-				<li>
-					<a href="#kontakty">Kontakty</a>
-				</li>
-				<li>
-					<a href="#cenik">Ceník</a>
-				</li>
-			</ul>
-		</>
-	)
-}
+  const [openMenu, getOpenMenu] = useState(false);
+
+  const handleClick = () => {
+    openMenu ? getOpenMenu(false) : getOpenMenu(true);
+  };
+
+  return (
+    <>
+      <button className="hamburger" aria-label="menu" onClick={handleClick}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      {openMenu ? (
+        <ul>
+          <li>
+            <a href="#o-nas">O nás</a>
+          </li>
+          <li>
+            <a href="#co-delame">Co děláme</a>
+          </li>
+          <li>
+            <a href="#kontakty">Kontakty</a>
+          </li>
+          <li>
+            <a href="#cenik">Ceník</a>
+          </li>
+        </ul>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
