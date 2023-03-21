a91ca71b527cabfccac8f1f6410466c963ba6f52
Marketa Cechova <123273503+markcech@users.noreply.github.com>
2023-03-22 00:20:33 +0100

    Update index.jsx

--- src/ZaverecnyBonus1/index.jsx
+++ src/ZaverecnyBonus1/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from "react";
 
 // Zadání 1: Pomocí události `change` na vstupním políčku a vlastnosti `event.target.value` ukládej do stavu, co uživatel napsal.
 // Zadání 2: Mezi tagy `<b></b>` vlož napsaný text.
@@ -6,16 +6,21 @@ import React from 'react'
 // Zadání 4. Poslední odstavec zobraz pouze v případě, že je text kratší než 8 znaků.
 
 export const ZaverecnyBonus1 = () => {
-	return (
-		<>
-			<label>
-				Napiš něco: <input />
-			</label>
-			<p>
-				Do políčka výše uživatel napsal: <b>@TODO</b>
-			</p>
-			<p>Počet znaků: 0</p>
-			<p>Jako heslo by text neobstál.</p>
-		</>
-	)
-}
+  const [text, setText] = useState("");
+
+  console.log(text);
+
+  return (
+    <>
+      <label>
+        Napiš něco:{" "}
+        <input type="text" onChange={(e) => setText(e.target.value)} />
+      </label>
+      <p>
+        Do políčka výše uživatel napsal: <b>{text}</b>
+      </p>
+      <p>Počet znaků: {text.length} </p>
+      {text.length <= 8 ? <p>Jako heslo by text neobstál.</p> : <></>}
+    </>
+  );
+};
